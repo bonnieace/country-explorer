f9f66d70b1e74435747a8c686ae8b3e6e89092ba
bonnieace <bonniemasota@gmail.com>
2025-02-20 11:52:53 +0300

    Initial Vue Setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    Country explorer
+  </main>
+</template>
+<style scoped>
+*{
+  text-align: center;
+  font-weight: bold;
+  font-size: larger;
+}
+
+</style>
